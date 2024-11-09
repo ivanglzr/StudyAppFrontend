38594297e582224fe940eac5d03704b4c7d3be79
Ivan <ivanglzr01@gmail.com>
2024-11-09 12:11:24 +0100

    feat: register

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,12 +3,24 @@
 import { redirect } from "next/navigation";
 
 import { IResponse, IBadResponse } from "@/interfaces/response.interfaces";
-import { ILogIn } from "@/interfaces/auth.interfaces";
+import { ILogIn, IRegister } from "@/interfaces/auth.interfaces";
 
 import { handleErrors, validateResponse } from "../utils";
 import { clearAccessToken, setAccessToken } from "../cookies";
 
 import { AUTH_ROUTES } from "./auth.routes";
+
+import { accessTokenCookieName } from "./auth.config";
+
+function extractAccessTokenFromHeaders(headers: Headers) {
+  const accessToken = headers
+    .getSetCookie()
+    .find((cookie) => cookie.startsWith(accessTokenCookieName))
+    ?.split(";")[0]
+    .split("=")[1];
+
+  return accessToken;
+}
 
 export async function logIn(loginData: ILogIn) {
   try {
@@ -20,12 +32,35 @@ export async function logIn(loginData: ILogIn) {
       body: JSON.stringify(loginData),
     });
 
-    const accessToken = petition.headers
-      .get("Set-Cookie")
-      ?.split(";")[0]
-      .split("=")[1];
+    const accessToken = extractAccessTokenFromHeaders(petition.headers);
 
     const res: IResponse | IBadResponse = await petition.json();
+
+    if (!accessToken) return res.message;
+
+    validateResponse(res);
+
+    await setAccessToken(accessToken);
+
+    return res.message;
+  } catch (error) {
+    await handleErrors(error);
+  }
+}
+
+export async function register(registerData: IRegister) {
+  try {
+    const petition = await fetch(AUTH_ROUTES.REGISTER, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registerData),
+    });
+
+    const res = await petition.json();
+
+    const accessToken = extractAccessTokenFromHeaders(petition.headers);
 
     if (!accessToken) return res.message;
 
