68cf164f4362511ea4dc4b544bb6344ee3426e08
Ivan <ivanglzr01@gmail.com>
2024-11-10 14:33:00 +0100

    feat: change password

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -3,7 +3,11 @@
 import { getAccessToken } from "../cookies";
 import { getCookieHeader, handleErrors, validateResponse } from "../utils";
 
-import { IGetUserResponse } from "@/interfaces/user.interfaces";
+import {
+  IChangePassword,
+  IGetUserResponse,
+} from "@/interfaces/user.interfaces";
+import { IResponse } from "@/interfaces/response.interfaces";
 
 import { USER_ROUTES } from "./user.routes";
 
@@ -23,6 +27,31 @@ export async function getUser() {
     validateResponse(res);
 
     return res.user;
+  } catch (error) {
+    await handleErrors(error);
+  }
+}
+
+export async function changeUserPassword(passwords: IChangePassword) {
+  const token = await getAccessToken(true);
+
+  //? Only to avoid typescript error
+  if (!token) return;
+
+  try {
+    const petition = await fetch(USER_ROUTES.CHANGE_PASSWORD, {
+      method: "PATCH",
+      headers: {
+        ...getCookieHeader(token),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(passwords),
+    });
+    const res: IResponse = await petition.json();
+
+    validateResponse(res);
+
+    return res.message;
   } catch (error) {
     await handleErrors(error);
   }
