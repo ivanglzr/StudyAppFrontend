3b110404e0203611b5e615f168f610654dd82bbf
Ivan <ivanglzr01@gmail.com>
2024-11-12 12:37:06 +0100

    chore: form group component finished

--- src/components/forms/FormGroup.tsx
+++ src/components/forms/FormGroup.tsx
@@ -1,14 +1,26 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ErrorSpan } from "./ErrorSpan";
 
 interface Props {
   type: React.HTMLInputTypeAttribute;
   inputName: string;
   label: string;
   placeholder?: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string | readonly string[] | number | undefined;
+  errorMessage: string | null;
 }
 
-export function FormGroup({ type, inputName, label, placeholder }: Props) {
+export function FormGroup({
+  type,
+  inputName,
+  label,
+  placeholder,
+  handleChange,
+  value,
+  errorMessage
+}: Props) {
   return (
     <div className="w-full mt-4">
       <Label className="text-sm" htmlFor={inputName}>
@@ -19,7 +31,10 @@ export function FormGroup({ type, inputName, label, placeholder }: Props) {
         name={inputName}
         id={inputName}
         placeholder={placeholder}
+        onChange={handleChange}
+        value={value}
       />
+      <ErrorSpan error={errorMessage}/>
     </div>
   );
 }
