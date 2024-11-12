c05ebb6b222eff95ca81fb40464b15f747813983
Ivan <ivanglzr01@gmail.com>
2024-11-12 18:06:11 +0100

    chore: component for alert message

---
+++ src/components/AlertMessage.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Info, AlertCircle, X } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+
+import { useAlertMessageStore } from "@/store/alertMessage";
+
+export function AlertMessage() {
+  const isVisible = useAlertMessageStore((state) => state.isVisible);
+  const props = useAlertMessageStore((state) => state.props);
+
+  const hideAlert = useAlertMessageStore((state) => state.hideAlert);
+
+  return (
+    <>
+      {isVisible && (
+        <div className="flex justify-center items-center absolute w-full h-full bg-gray-50 bg-opacity-75">
+          <Alert
+            className={`relative min-w-80 w-max shadow-lg shadow-gray-500 border-2 ${
+              props.variant === "default" ? "border border-black" : ""
+            }`}
+            variant={props.variant}
+          >
+            {props.variant === "default" ? (
+              <Info size={20} />
+            ) : (
+              <AlertCircle size={20} />
+            )}
+            <AlertTitle className="text-lg font-bold">{props.title}</AlertTitle>
+            <AlertDescription>{props.message}</AlertDescription>
+
+            <button
+              onClick={hideAlert}
+              className="absolute top-0 right-0 mr-2 mt-2 cursor-pointer"
+            >
+              <X size={20} strokeWidth={1.5} />
+            </button>
+          </Alert>
+        </div>
+      )}
+    </>
+  );
+}
