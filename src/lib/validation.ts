79bc4408b621b6cb0d42ee65bb775368ea3784dc
Ivan <ivanglzr01@gmail.com>
2024-11-12 09:18:01 +0100

    chore: validation for log in

---
+++ src/lib/validation.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+import { ILogIn } from "@/interfaces/auth.interfaces";
+
+const passwordRegex =
+  /^(?=(.*[A-Z]){2,})(?=(.*[a-z]){2,})(?=(.*\d){1,})(?=(.*[!@#$%^&*()_\-+=[\]{};':"\\|,.<>/?]){1,}).{8,}$/;
+
+const emailSchema = z
+  .string({
+    invalid_type_error: "Email must be a string",
+    required_error: "Email is required",
+  })
+  .email("Email isn't valid")
+  .min(1, "Email can't be empty")
+  .min(5, "Email must be atleast 5 characters long")
+  .max(50, "Email can't have more than 50 characters");
+
+const passwordSchema = z
+  .string({
+    required_error: "Password is required",
+    invalid_type_error: "Password must be an string",
+  })
+  .min(1, "Password can't be empty")
+  .min(8, "Password must be atleast 8 characters long")
+  .max(50, "Password can't have more than 50 characters")
+  .regex(
+    passwordRegex,
+    "Password must have 2 uppercase letters, 2 lowercase letters, 2 numbers and a symbol"
+  );
+
+const logInSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+});
+
+export const validateLogInSchema = (loginData: ILogIn) =>
+  logInSchema.safeParse(loginData);
