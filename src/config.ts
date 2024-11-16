dc8a052ffa84489bc80627390d89097a507abe45
Ivan <ivanglzr01@gmail.com>
2024-11-16 14:58:59 +0100

    chore: global config

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  AUTH_ENDPOINT: "/auth",
+  LOG_IN: "/auth/login",
+  REGISTER: "/auth/register",
+  HOME: "/home",
+  LIBRARY: "/library",
+};
+
+export const accessTokenCookieName = "access_token";
