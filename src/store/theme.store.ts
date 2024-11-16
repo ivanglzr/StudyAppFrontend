335321d3c36d9bb3b48a3c392360616d336550b6
Ivan <ivanglzr01@gmail.com>
2024-11-17 00:21:07 +0100

    chore: add a dark mode property to store and save it local storage

--- src/store/theme.store.ts
+++ src/store/theme.store.ts
@@ -5,21 +5,79 @@ import { create } from "zustand";
 export const themeColors = ["blue", "green", "purple"] as const;
 export type themeColorsType = (typeof themeColors)[number];
 
+interface Theme {
+  darkMode: boolean;
+  colorTheme: themeColorsType | undefined;
+}
+
 interface Props {
-  theme: themeColorsType | undefined;
+  theme: Theme;
 }
 
 interface Actions {
-  setTheme: (theme: themeColorsType) => void;
+  setTheme: (newTheme: Partial<Theme>) => void;
+  setInitialTheme: () => void;
+}
+
+function getThemeFromLocalStorage() {
+  const theme = window?.localStorage.getItem("theme");
+
+  if (!theme) return undefined;
+
+  const parsedTheme = JSON.parse(theme) as Theme;
+
+  return parsedTheme;
+}
+
+function setInitialTheme() {
+  const theme = getThemeFromLocalStorage();
+
+  if (!theme) return;
+
+  if (theme.darkMode) document.body.classList.add("dark");
+  else document.body.classList.remove("dark");
+
+  if (!theme.colorTheme) return;
+
+  document.body.classList.add(`theme-${theme.colorTheme}`);
+
+  return theme;
+}
+
+function saveThemeInLocalStorage(theme: Theme) {
+  window.localStorage.setItem("theme", JSON.stringify(theme));
 }
 
 export const useThemeStore = create<Props & Actions>((set, get) => ({
-  theme: undefined,
-  setTheme: (theme: themeColorsType) => {
-    document.body.classList.remove(`theme-${get().theme}`);
+  theme: {
+    darkMode: true,
+    colorTheme: undefined,
+  },
+  setTheme: (newTheme: Partial<Theme>) => {
+    const actualTheme = get().theme;
+
+    if (Object.hasOwn(newTheme, "darkMode") && newTheme.darkMode)
+      document.body.classList.add("dark");
+    else if (Object.hasOwn(newTheme, "darkMode") && !newTheme.darkMode)
+      document.body.classList.remove("dark");
+
+    if (newTheme.colorTheme && newTheme.colorTheme !== actualTheme.colorTheme) {
+      document.body.classList.remove(`theme-${actualTheme.colorTheme}`);
+
+      set({ theme: { ...actualTheme, ...newTheme } });
+
+      document.body.classList.add(`theme-${newTheme.colorTheme}`);
+    }
+
+    set({ theme: { ...actualTheme, ...newTheme } });
+
+    saveThemeInLocalStorage(get().theme);
+  },
+  setInitialTheme: () => {
+    const theme = setInitialTheme();
+
+    if (!theme) return;
 
     set({ theme });
-
-    document.body.classList.add(`theme-${get().theme}`);
   },
 }));
