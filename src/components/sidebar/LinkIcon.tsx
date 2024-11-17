a233d6300f138e4d8d52fe6c1c2bafc04588ef80
Ivan <ivanglzr01@gmail.com>
2024-11-17 19:54:31 +0100

    chore: link icon for sidebar

---
+++ src/components/sidebar/LinkIcon.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+interface Props {
+  icon: (color: string) => React.ReactNode;
+  href: string;
+  text: string;
+  isActive: boolean;
+}
+
+export function LinkIcon({ icon, href, text, isActive }: Props) {
+  const iconColor = isActive
+    ? "hsl(var(--primary-foreground))"
+    : "hsl(var(--primary))";
+
+  const textColor = isActive ? "text-primary-foreground" : "text-primary";
+
+  return (
+    <Link
+      href={href}
+      className={`flex flex-col justify-center items-center p-2 rounded-lg ${
+        isActive ? "bg-primary" : ""
+      }`}
+    >
+      <div className="rounded-lg">{icon(iconColor)}</div>
+      <span className={`text-sm opacity-90 ${textColor}`}>{text}</span>
+    </Link>
+  );
+}
