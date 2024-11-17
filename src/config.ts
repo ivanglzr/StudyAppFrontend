a497d8de0ac5bbabcebb398bb769bee29d237504
Ivan <ivanglzr01@gmail.com>
2024-11-17 09:56:01 +0100

    chore: add the user endpoint to home and library

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,9 @@ export const ROUTES = {
   AUTH_ENDPOINT: "/auth",
   LOG_IN: "/auth/login",
   REGISTER: "/auth/register",
-  HOME: "/home",
-  LIBRARY: "/library",
+  USER_ENDPOINT: "/home",
+  HOME: "/user/home",
+  LIBRARY: "/user/library",
 };
 
 export const accessTokenCookieName = "access_token";
