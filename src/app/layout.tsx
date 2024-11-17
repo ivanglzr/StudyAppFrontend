81a4749776954c94a6b1661a20553f58b4f36673
Ivan <ivanglzr01@gmail.com>
2024-11-17 10:55:38 +0100

    fix: set body height to screen height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={`min-h-screen ${poppins.className}`}>
         {children}
         <AlertMessage />
         <ThemeManager />
