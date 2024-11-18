dcfa64bbe9c3e4cbc976ad3a9792f3d2260126d7
Ivan <ivanglzr01@gmail.com>
2024-11-18 18:51:43 +0100

    chore: subject interfaces

---
+++ src/interfaces/subject.interfaces.ts
@@ -0,0 +1,23 @@
+import { IResponse } from "./response.interfaces";
+
+import { IFlashcard } from "./flashcard.interfaces";
+import { INote } from "./note.interfaces";
+import { IExam } from "./exam.interfaces";
+
+export interface ISubject {
+  userId: string;
+  subjectName: string;
+  flashcards: IFlashcard[];
+  notes: INote[];
+  exams: IExam[];
+  documents: string[];
+  color: string;
+}
+
+export interface IGetSubjectsResponse extends IResponse {
+  subjects: ISubject[];
+}
+
+export interface IGetSubjectResponse extends IResponse {
+  subject: ISubject;
+}
