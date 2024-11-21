c25183db04ba08e2388512a44094d6ca66b4df45
Ivan <ivanglzr01@gmail.com>
2024-11-21 19:16:38 +0100

    feat: change library's title border styles

--- src/components/library/Library.tsx
+++ src/components/library/Library.tsx
@@ -11,11 +11,9 @@ export async function Library() {
   if (subjects.length === 0)
     return <span>You don&apos;t have any subjects, create one!</span>;
 
-  console.log(subjects[0]);
-
   return (
     <>
-      <h1 className="m-2 mb-6 text-4xl text-primary border-b-2 border-b-primary/90 opacity-90">
+      <h1 className="m-2 mb-6 text-4xl border-b-2 border-b-foreground/75 opacity-90">
         Subjects
       </h1>
       <section className="grid grid-cols-4 gap-4 mx-2">
