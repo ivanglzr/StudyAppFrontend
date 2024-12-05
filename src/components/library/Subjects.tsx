3a09396ed8922b77b2a2669fd11b34a2cb043b26
Ivan <ivanglzr01@gmail.com>
2024-12-05 18:18:35 +0100

    chore: subjects component

---
+++ src/components/library/Subjects.tsx
@@ -0,0 +1,23 @@
+import { Subject } from "./Subject";
+
+import { ISubject } from "@/interfaces/subject.interfaces";
+
+interface Props {
+  subjects: ISubject[] | undefined;
+}
+
+export function Subjects({ subjects }: Props) {
+  if (subjects === undefined)
+    return <span>An error ocurred while loading the subjects</span>;
+
+  if (subjects.length === 0)
+    return <span>You don&apos;t have any subjects, create one!</span>;
+
+  return (
+    <section className="grid grid-cols-4 gap-4 mx-2">
+      {subjects.map((subject) => (
+        <Subject subject={subject} key={subject._id} />
+      ))}
+    </section>
+  );
+}
