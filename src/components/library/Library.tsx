2675c985de3c9604cc3bca33ddb3379c4bb412a9
Ivan <ivanglzr01@gmail.com>
2024-12-06 17:34:28 +0100

    refactor: use header component

--- src/components/library/Library.tsx
+++ src/components/library/Library.tsx
@@ -1,17 +1,18 @@
 import { CreateSubjectDialog } from "./CreateSubjectDialog";
+import { Subjects } from "./Subjects";
+import { Header } from "../header/Header";
 
 import { getSubjects } from "@/services/subject/subject.service";
-import { Subjects } from "./Subjects";
 
 export async function Library() {
   const subjects = await getSubjects();
 
   return (
     <>
-      <header className="flex m-2 mb-6 text-4xl border-b-2 border-b-foreground/75 opacity-90">
+      <Header>
         <h1>Subjects</h1>
         <CreateSubjectDialog />
-      </header>
+      </Header>
       <Subjects subjects={subjects} />
     </>
   );
