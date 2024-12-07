2eb8c60a1c3a6665a01b57d4de21016ed4f70b10
Ivan <ivanglzr01@gmail.com>
2024-12-07 19:12:32 +0100

    chore: route for updating a subject's study time

--- src/services/stats/stats.routes.ts
+++ src/services/stats/stats.routes.ts
@@ -6,4 +6,6 @@ if (!BACKEND_URL)
 export const STATS_ROUTES = {
   GET_STATS: BACKEND_URL + "/user/stats",
   GET_SUBJECTS_STATS: BACKEND_URL + "/user/stats/subject",
+  UPDATE_SUBJECT_STUDY_TIME: (subjectId: string) =>
+    `${BACKEND_URL}/user/stats/subject/${subjectId}`,
 };
