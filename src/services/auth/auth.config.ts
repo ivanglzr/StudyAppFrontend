e4a048fc43a3b5858febd0105a59ab789a7a2392
Ivan <ivanglzr01@gmail.com>
2024-12-12 21:29:50 +0100

    chore: use node env to set if cookie is secure

--- src/services/auth/auth.config.ts
+++ src/services/auth/auth.config.ts
@@ -1,7 +1,7 @@
 import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 export const cookieOptions: Partial<ResponseCookie> = {
-  secure: true,
+  secure: process.env.NODE_ENV === "production",
   httpOnly: true,
   sameSite: "lax",
   expires: new Date(Date.now() + 1000 * 60 * 60),
