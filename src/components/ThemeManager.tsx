33f7a2835c0c0d2ece22d094f27dddb75d55f9b0
Ivan <ivanglzr01@gmail.com>
2024-12-12 23:05:37 +0100

    feat: buttons to update the theme

--- src/components/ThemeManager.tsx
+++ src/components/ThemeManager.tsx
@@ -1,12 +1,46 @@
 "use client";
 
-import { useThemeStore } from "@/store/theme.store";
 import { useEffect } from "react";
+
+import { Moon, Sun } from "lucide-react";
+
+import { useThemeStore, themeColors } from "@/store/theme.store";
+
+const iconProps = {
+  size: 40,
+  strokeWidth: 1.5,
+};
 
 export function ThemeManager() {
   const setInitialTheme = useThemeStore((state) => state.setInitialTheme);
+  const toggleDarkMode = useThemeStore((state) => state.toggleDarkMode);
+  const setColorTheme = useThemeStore((state) => state.setColorTheme);
 
-  useEffect(setInitialTheme, [setInitialTheme]);
+  const theme = useThemeStore((state) => state.theme);
 
-  return false;
+  useEffect(() => setInitialTheme(), []);
+
+  return (
+    <div className="flex items-center gap-6 absolute bottom-0 right-0 m-6 rounded-lg">
+      <div className="flex gap-2">
+        {themeColors.map((themeColor) => (
+          <button
+            key={themeColor}
+            className="w-6 h-6 rounded-sm"
+            style={{ backgroundColor: themeColor }}
+            onClick={() => setColorTheme(themeColor)}
+          />
+        ))}
+      </div>
+      {theme.darkMode ? (
+        <button onClick={toggleDarkMode}>
+          <Moon {...iconProps} />
+        </button>
+      ) : (
+        <button onClick={toggleDarkMode}>
+          <Sun {...iconProps} />
+        </button>
+      )}
+    </div>
+  );
 }
