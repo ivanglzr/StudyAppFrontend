64d9c1087ece59894c3a7c803d7a43ea6dc3533e
Ivan <ivanglzr01@gmail.com>
2024-12-18 20:30:05 +0100

    chore: file icons

---
+++ src/document/components/Icons.tsx
@@ -0,0 +1,98 @@
+import { SVGProps } from "react";
+
+export const WordIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+  >
+    <path
+      d="M9.5 29h19c.275 0 .5-.225.5-.5V9h-4.5c-.827 0-1.5-.673-1.5-1.5V3H9.5c-.275 0-.5.225-.5.5v25c0 .275.225.5.5.5z"
+      fill="#fff"
+    />
+    <path d="M28.293 8 24 3.707V7.5c0 .275.225.5.5.5h3.793z" fill="#fff" />
+    <path
+      opacity=".67"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m29.56 7.854-5.414-5.415A1.51 1.51 0 0 0 23.086 2H9.5C8.673 2 8 2.673 8 3.5v25c0 .827.673 1.5 1.5 1.5h19c.827 0 1.5-.673 1.5-1.5V8.914c0-.4-.156-.777-.44-1.06zM24 3.707 28.293 8H24.5a.501.501 0 0 1-.5-.5V3.707zM9.5 29h19c.275 0 .5-.225.5-.5V9h-4.5c-.827 0-1.5-.673-1.5-1.5V3H9.5c-.275 0-.5.225-.5.5v25c0 .276.224.5.5.5z"
+      fill="#605E5C"
+    />
+    <path d="M25.5 22H17v1h8.5a.5.5 0 0 0 0-1z" fill="#103F91" />
+    <path d="M25.5 19H17v1h8.5a.5.5 0 0 0 0-1z" fill="#185ABD" />
+    <path d="M25.5 16H17v1h8.5a.5.5 0 0 0 0-1z" fill="#2B7CD3" />
+    <path d="M25.5 13H17v1h8.5a.5.5 0 0 0 0-1z" fill="#41A5EE" />
+    <path
+      d="M3.5 25h11a1.5 1.5 0 0 0 1.5-1.5v-11a1.5 1.5 0 0 0-1.5-1.5h-11A1.5 1.5 0 0 0 2 12.5v11A1.5 1.5 0 0 0 3.5 25z"
+      fill="#185ABD"
+    />
+    <path
+      d="M7.215 20.73h.022C7.246 20.615 8.37 15 8.37 15h1.306s1.14 5.353 1.175 5.72h.017c.015-.252.944-5.72.944-5.72H13l-1.462 7h-1.389s-1.153-5.53-1.162-5.628h-.018C8.959 16.485 7.886 22 7.886 22h-1.41L5 15h1.21s1 5.607 1.005 5.73z"
+      fill="#F9F7F7"
+    />
+  </svg>
+);
+
+export const PDFIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    {...props}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+  >
+    <path
+      d="M6.5 29h19c.275 0 .5-.225.5-.5V9h-4.5c-.827 0-1.5-.673-1.5-1.5V3H6.5c-.275 0-.5.225-.5.5v25c0 .275.225.5.5.5z"
+      fill="#fff"
+    />
+    <path d="M25.293 8 21 3.707V7.5c0 .275.225.5.5.5h3.793z" fill="#fff" />
+    <path
+      opacity=".67"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="m26.56 7.854-5.414-5.415A1.51 1.51 0 0 0 20.086 2H6.5C5.673 2 5 2.673 5 3.5v25c0 .827.673 1.5 1.5 1.5h19c.827 0 1.5-.673 1.5-1.5V8.914c0-.4-.156-.777-.44-1.06zM21 3.707 25.293 8H21.5a.501.501 0 0 1-.5-.5V3.707zM6.5 29h19c.275 0 .5-.225.5-.5V9h-4.5c-.827 0-1.5-.673-1.5-1.5V3H6.5c-.275 0-.5.225-.5.5v25c0 .276.224.5.5.5z"
+      fill="#605E5C"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M23.5 13h-15a.5.5 0 0 1 0-1h15a.5.5 0 0 1 0 1zm0 2h-15a.5.5 0 0 1 0-1h15a.5.5 0 0 1 0 1zm-15 2h15a.5.5 0 0 0 0-1h-15a.5.5 0 0 0 0 1z"
+      fill="#C8C6C4"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13 19h6a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2zm7 2c0-.551-.449-1-1-1h-6c-.551 0-1 .449-1 1v4c0 .551.449 1 1 1h6c.551 0 1-.449 1-1v-4zM5 26h4.75a.25.25 0 0 0 .25-.25v-5.5a.25.25 0 0 0-.25-.25H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1zm17.25-6H27a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1h-4.75a.25.25 0 0 1-.25-.25v-5.5a.25.25 0 0 1 .25-.25z"
+      fill="#D65532"
+    />
+  </svg>
+);
+
+export const Default = (props: SVGProps<SVGSVGElement>) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+    <g stroke="#979593" strokeMiterlimit="10">
+      <path
+        fill="#FFF"
+        d="M20.086 2.5H6.5a1 1 0 00-1 1v25a1 1 0 001 1h19a1 1 0 001-1V8.914a1 1 0 00-.293-.707l-5.414-5.414a1 1 0 00-.707-.293z"
+      />
+      <path fill="none" d="M20.5 2.5v5a1 1 0 001 1h5" />
+    </g>
+  </svg>
+);
+
+const iconsMap: {
+  [key: string]: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+} = {
+  ".docx": (props) => <WordIcon {...props} />,
+  ".pdf": (props) => <PDFIcon {...props} />,
+  default: (props) => <Default {...props} />,
+};
+
+export const Icon = ({
+  ext,
+  ...props
+}: SVGProps<SVGSVGElement> & { ext: string }) => {
+  const IconComponent = iconsMap[ext] ?? iconsMap.default;
+
+  return <IconComponent {...props} />;
+};
