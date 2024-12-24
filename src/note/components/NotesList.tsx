b1b27d5c9fffa99b6eb7c4952c68bd050bb19d6e
Ivan <ivanglzr01@gmail.com>
2024-12-24 15:20:38 +0100

    chore: notes list

---
+++ src/note/components/NotesList.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { Button } from "@/common/components/ui";
+import { NoteItem } from ".";
+
+import { INote } from "../interfaces";
+
+import { ROUTES } from "@/config";
+
+interface Props {
+  notes: INote[];
+  subjectId: string;
+}
+
+export function NotesList({ notes, subjectId }: Props) {
+  return (
+    <div className="mt-4 pt-2 border-t-2 border-t-background">
+      <header className="flex justify-between">
+        <h2 className="mb-2 text-2xl">Notes</h2>
+        <Link href={ROUTES.CREATE_NOTE_PAGE(subjectId)}>
+          <Button>Create a Note</Button>
+        </Link>
+      </header>
+      <ul>
+        {notes.map((note) => (
+          <NoteItem note={note} subjectId={subjectId} />
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/note/components/index.ts
+++ src/note/components/index.ts
@@ -1,2 +1,4 @@
 export { default as NoteEditor } from "./NoteEditor";
 export * from "./NoteEditor";
+export * from "./NotesList";
+export * from "./NoteItem";
