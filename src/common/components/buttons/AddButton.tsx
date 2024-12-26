c2726a2a5d5f103873fb0bbcc94e15501a6e19e8
Ivan <ivanglzr01@gmail.com>
2024-12-26 13:41:28 +0100

    chore: button for dialogs

---
+++ src/common/components/buttons/AddButton.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes, forwardRef } from "react";
+
+import { Plus } from "lucide-react";
+import { Button } from "../ui";
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+}
+
+export const AddButton = forwardRef<HTMLButtonElement, Props>(
+  ({ text, className, ...props }, ref) => {
+    return (
+      <Button ref={ref} className={`ml-auto ${className}`} {...props}>
+        <span className="hidden sm:inline-block">{text}</span>
+        <Plus className="inline-block sm:hidden" width={40} height={40} />
+      </Button>
+    );
+  }
+);
+
+AddButton.displayName = "AddButton";
